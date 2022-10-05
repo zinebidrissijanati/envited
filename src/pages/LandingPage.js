a616d05707667e57097481830e240728993e8bb2
zinebidrissijanati <60932655+zinebidrissijanati@users.noreply.github.com>
2022-10-05 11:33:12 +0000

    feat: landing page for small devices

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,7 +1,20 @@
+import { GiPartyPopper } from 'react-icons/gi';
 export default function LandingPage() {
     return (
-      <h1 className="text-3xl font-bold underline mt-10">
-        Hello world!
-      </h1>
+        <div className="mt-24">
+            <div className="h-28 font-bold font-sans text-4xl text-center leading-9">
+                <p className="text-first-color">Imagine If</p>
+                <p className="text-transparent bg-clip-text bg-gradient-to-r from-blue-magenta to-atomic-pink-color">Snapchat</p>
+                <p className="text-first-color">had events.</p>
+            </div>
+            <p className="font-sans text-gray-color mt-4 text-base text-center font-light">Easily host and share events with your<br /> friends across any social media.</p>
+            <div className="grid justify-items-stretch">
+                <img className="justify-self-center mt-9 w-44 h-72" alt="landing page" src={require("../assets/landing.jpg")}></img>
+                <button className="inline-flex items-center justify-center text-white font-bold font-sans text-base p-4 justify-self-center  bg-gradient-to-r mt-8 h-12 w-48 mb-4 rounded-lg from-blue-magenta to-atomic-pink-color">
+                    <GiPartyPopper className="mr-1" /> <span>Create my event</span>
+                </button>
+            </div>
+                
+        </div>
     )
-  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'first-color': '#240D57',
+        'blue-magenta': '#8456EC',
+        'atomic-pink-color': '#E87BF8',
+        'gray-color': '#4F4F4F'
+      },
+      fontFamily: {
+        'sans': ['Helvetica']
+      }
+    },
   },
   plugins: [],
 }
